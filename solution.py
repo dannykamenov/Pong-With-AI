eef611ce414632ae6f02ae493d03e2548076109e
Daniel Kamenov <sorostmcoc@gmail.com>
2023-09-22 14:30:38 +0300

    initializing neat

--- solution.py
+++ solution.py
@@ -1,4 +1,6 @@
 import pygame
+import neat
+import os
 
 pygame.init()
 
@@ -160,5 +162,19 @@ def main():
     
     pygame.quit()
 
+#def eval_genomes(genomes, config):
+            
+def run_neat(config):
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+    #winner = p.run(eval_genomes, 50)
+
 if __name__ == "__main__":
     main()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    run_neat(config)
